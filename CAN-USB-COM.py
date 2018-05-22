3ab4611bdafd481c8507b6000f79f54c5bb2cbbe
rdslade <rdslade@umich.edu>
2018-05-22 13:49:00 -0500

    added instructions and both com ports to display

--- CAN-USB-COM.py
+++ CAN-USB-COM.py
@@ -5,16 +5,17 @@ from time import sleep
 import serial
 
 class Port:
-    def __init__(self, parent, comin):
+    def __init__(self, parent, prog_com_, out_com_):
         self.parent = parent
-        self.com = comin
+        self.prog_com = prog_com_
+        self.out_com = out_com_
         self.frame = tk.Frame(self.parent)
         self.initTitle()
         self.packObjects()
 
     def initTitle(self):
-        self.instructions = tk.Label(self.frame, text = self.com)
-        self.statusSpace = tk.LabelFrame(self.frame, text = 'START', width = 100, height = 200)
+        self.instructions = tk.Label(self.frame, text = self.prog_com + "\\" + self.out_com)
+        self.statusSpace = tk.LabelFrame(self.frame, width = 200, height = 250)
 
     def packObjects(self):
         self.instructions.pack()
@@ -24,30 +25,40 @@ class Port:
 def getCOMPorts():
     try:
         with open("config.txt", 'r',encoding='utf-8' ) as mp:
-            line = mp.readline()
-            ports = []
-            for p in line.split():
-                if "COM" in p:
-                    ports.append(p)
-        return ports
+            mp.readline()
+            devices = []
+            for line in mp.readlines():
+                ports = []
+                for p in line.split():
+                    if "COM" in p:
+                        ports.append(p)
+                devices.append(ports)
+        return devices
     except IOError:
         messagebox.showinfo("IOError", "Missing config.txt file")
 
 class Application:
     def __init__(self, parent):
         self.parent = parent
+        self.parent.title("CAN-232 Programmer")
+        self.parent.geometry("600x400")
         self.frame = tk.Frame(self.parent)
-        self.label = tk.Label(self.frame, text = "Instructions")
-        self.label.pack()
+        self.titleLabel = tk.Label(self.frame, text = 'Details/Instructions', font = ("Times", 16))
+        self.instructions = tk.Label(self.frame, text = '- Programming stations \
+are labelled with both COM ports listed in config.txt\n \
+            - Click START to begin the upload and verification')
+        self.start = tk.Button(self.frame, text = "START", width = 10, height = 2);
+        self.packObjects()
+        devices = getCOMPorts()
+        for d in devices:
+            Port(root, d[0], d[1])
+    def packObjects(self):
+        self.titleLabel.pack()
+        self.instructions.pack()
         self.frame.pack()
-        ports = getCOMPorts()
-        for p in ports:
-            Port(root, p)
-
+        self.start.pack()
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("CAN-232 Programmer")
-    root.geometry("600x300")
     a1 = Application(root)
     root.mainloop()
